0d6b9d30ce81364858e205cea77d8033c2d86110
Lucas Matheus <lmsl3@aluno.ifal.edu.br>
2026-02-04 03:49:45 -0300

    Fixed a problem with the function that clears filters in the form: it was redirecting to another page.

--- src/app/_components/form.jsx
+++ src/app/_components/form.jsx
@@ -41,7 +41,6 @@ export function Form({ alwaysShowFilters = false }) {
 
     const handleClearFilters = () => {
         setFormData(initialFormState);
-        router.push('/search');
     };
 
     const handleSubmit = (e) => {
